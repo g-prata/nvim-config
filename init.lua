22343ed4d7cc664a9fad65bbea90c062eb612805
gprata <gprata@outlook.pt>
2024-11-07 21:22:06 +0000

    disable mouse in every mode

--- init.lua
+++ init.lua
@@ -108,7 +108,7 @@ vim.opt.relativenumber = true
 vim.opt.guicursor = ''
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
